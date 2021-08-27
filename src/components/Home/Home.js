f5a05d551ffd68654670d92422cfa0887735a788
calmighted <calmighted@gmail.com>
2021-08-27 19:21:29 +0530

    added styles to notice

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react';
-import Nav from '../Nav/Nav';
-import "./Home.css"
-import {fetchNotice} from "../../api/index"
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Nav from "../Nav/Nav";
+import "./Home.css";
+import { fetchNotice } from "../../api/index";
 
 
 
@@ -17,46 +17,48 @@ function Home() {
 	// 	}
 	// })
 
-	const [noticelist, setNoticelist] = useState([])
-
-
-
+	const [noticelist, setNoticelist] = useState([]);
 
 	useEffect(() => {
-		async function getNotice(){
+		async function getNotice() {
 			try {
 				const { data } = await fetchNotice;
-				setNoticelist(data)
+				setNoticelist(data);
 			} catch (error) {
 				console.log(error);
 			}
-		};
-		getNotice()
-	},[])
+		}
+		getNotice();
+	}, []);
 
-
-    return (
-				<div className="App">
-                    <Nav />
-					<p className="org">MES COLLEGE OF ENGINEERING</p>
-					<div className="noticeBoard">
-						{
-							noticelist.map(notice => (
-								<div className="notice" key={notice._id}>
-									{console.log(noticelist)}
-									<div className="noticeContent">
-										<span className="heading">{notice.title}</span>
-										<span className="noticeText">{notice.description}</span>
-										<span className="creator">{notice.creator}</span>
-										<span className="createdAt">{notice.createdAt}</span>
-									</div>
-									<div className="reactions"></div>
-								</div>
-							))
-						}
+	return (
+		<div className="App">
+			<Nav />
+			<p className="org">MES COLLEGE OF ENGINEERING</p>
+			<div className="noticeBoard">
+				{noticelist.map((notice) => (
+					<div className="notice" key={notice._id}>
+						{console.log(noticelist)}
+						<div className="noticeContent">
+							<span className="heading">{notice.title}</span>
+							<div className="noticeInfo">
+								<span className="creator">By {notice.creator}</span>
+								<span className="createdAt">{notice.createdAt}</span>
+							</div>
+							<span className="noticeText">{notice.description}</span>
+						</div>
+						{/* <div className="reactions">
+							<FacebookSelector
+								iconSize={24}
+								onSelect={() => console.log()}
+								reactions={["like", "wow", "sad", "angry"]}
+							/>
+						</div> */}
 					</div>
-				</div>
-		);
+				))}
+			</div>
+		</div>
+	);
 }
 
-export default Home
+export default Home;

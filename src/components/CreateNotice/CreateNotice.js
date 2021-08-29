2f19be193126fd6e71034eef6d5bba7bd38494f4
calmighted <calmighted@gmail.com>
2021-08-29 12:32:02 +0530

    added delete notice and update notice feature/buttons

--- src/components/CreateNotice/CreateNotice.js
+++ src/components/CreateNotice/CreateNotice.js
@@ -1,24 +1,49 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./CreateNotice.module.css";
 import Nav from "../Nav/Nav";
-import { fetchNotice } from "../../api/index";
+import {
+	fetchNotice,
+	createNotice,
+	deleteNotice,
+	updateNotice,
+} from "../../api/index";
+
+//two problems
+//1. update below posts after creating new notice or updating a new notice
 
 function CreateNotice() {
-		const [noticelist, setNoticelist] = useState([]);
+	const [noticelist, setNoticelist] = useState([]);
+	const [id, setid] = useState(null);
+	const [Notice, setNotice] = useState({
+		title: "",
+		description: "",
+		creator: "Principal",
+		tags: [],
+	});
 
-		useEffect(() => {
-			async function getNotice() {
-				try {
-					const { data } = await fetchNotice;
-					setNoticelist(data);
-				} catch (error) {
-					console.log(error);
-				}
+	useEffect(() => {
+		async function getNotice() {
+			try {
+				const { data } = await fetchNotice;
+				setNoticelist(data);
+			} catch (error) {
+				console.log(error);
 			}
-			getNotice();
-		}, []);
+		}
+		getNotice();
+	}, []);
 
-
+	const makeNewNotice = () => {
+		if (id) {
+			console.log(Notice);
+			updateNotice(id, Notice);
+		} else if (!Notice.title) {
+			alert("Title cant be empty");
+		} else {
+			console.log(Notice);
+			createNotice(Notice);
+		}
+	};
 
 	return (
 		<div className={styles.container}>
@@ -27,27 +52,48 @@ function CreateNotice() {
 				<form>
 					<div className={styles.formField}>
 						<label htmlFor="Title">Title</label>
-						<input type="text" />
+						<input
+							type="text"
+							value={Notice.title ? Notice.title : ""}
+							onChange={(e) => setNotice({ ...Notice, title: e.target.value })}
+						/>
 					</div>
 					<div className={`${styles.formField} ${styles.description}`}>
 						<label htmlFor="Title">Description</label>
-						<textarea className={styles.textarea}></textarea>
+						<textarea
+							className={styles.textarea}
+							value={Notice.description ? Notice.description : ""}
+							onChange={(e) =>
+								setNotice({ ...Notice, description: e.target.value })
+							}
+						></textarea>
 					</div>
 				</form>
 				<button
 					onClick={() => {
-						console.log("Hello");
+						makeNewNotice();
 					}}
 					className={styles.publishBtn}
 				>
-					Publish
+					{id ? "Re-Publish" : "Publish"}
 				</button>
 			</div>
 			<div className={styles.createdNotice}>
-				<h1>Created notice</h1>
+				<p className={styles.tag}>Created notice</p>
 				{noticelist.map((notice) => (
-					<div className="notice" key={notice._id}>
-						{console.log(noticelist)}
+					<div className={styles.notice} key={notice._id}>
+						<div className={styles.options}>
+							<button onClick={() => deleteNotice(notice._id)}>DELETE</button>
+							<button
+								onClick={() => {
+									console.log(notice._id);
+									setid(notice._id);
+									setNotice(notice);
+								}}
+							>
+								UPDATE
+							</button>
+						</div>
 						<div className="noticeContent">
 							<span className="heading">{notice.title}</span>
 							<div className="noticeInfo">
